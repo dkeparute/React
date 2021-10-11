4020e33ee65f2933cc2a12cc9fedecf6c815d00b
Dovilė <dovile.keparute@gmail.com>
2021-10-11 13:03:06 +0300

    start 1

--- src/start/index.js
+++ src/start/index.js
@@ -60,14 +60,52 @@
 
 
 // PATOBULINTAS DIVAS PAVADINIMU ELEMENT:
-const Element = (textNote, kokioTipoElementas, className) => {
-    const div = document.createElement(kokioTipoElementas); //1. sukuriamas tuscias divas, araba p, arba h1 bet jo dar nera narsykleje
-    const text = document.createTextNode(textNote); //4. sukuriame lipduka su tam tikru tekstu, bet jo dar nesimato narsykleje
-    div.appendChild(text); // 5. uzklijuojame ta lipduka su tekstu naryskleje
-    const root = document.querySelector('#root'); // 2. surandame vieta kur divas matysis narsykleje
-    root.appendChild(div); //3. pridedame div i matoma narsykleje vieta
-    div.classList.add(className); //prideda css klase js
+
+// const Element = (textNote, kokioTipoElementas, className) => {
+//     const div = document.createElement(kokioTipoElementas); //1. sukuriamas tuscias divas, araba p, arba h1 bet jo dar nera narsykleje
+//     const text = document.createTextNode(textNote); //4. sukuriame lipduka su tam tikru tekstu, bet jo dar nesimato narsykleje
+//     div.appendChild(text); // 5. uzklijuojame ta lipduka su tekstu naryskleje
+//     const root = document.querySelector('#root'); // 2. surandame vieta kur divas matysis narsykleje
+//     root.appendChild(div); //3. pridedame div i matoma narsykleje vieta
+//     div.classList.add(className); //prideda css klase js
+// }
+
+// SET STATE
+
+let state;
+
+const setState = (def = true) => {
+    if (def) {
+        state = 0;
+    } else {
+        state++;
+    }
+    console.log(state);
+    document.querySelector('#root').innerHTML = "";
+    data.forEach((s, nr) => {
+        if (state >= nr) {
+            Elem(s, 'h1', nr % 2 === 0 ? 'pink' : 'goth')
+        }
+    });
 }
-const data = ['Labas, Bebrai', 'Nu sveikutis, Barsukai', 'Kur aini', 'Namo ainu'];
-data.forEach((elementas, nr) => Element(elementas, "h1", nr%2? "pink": "goth"));
+
+document.querySelector('button').addEventListener('click', () => {
+    setState(false);
+})
+
+const Elem = (t, el, className) => {
+    const div = document.createElement(el); // sukuria tuscia div arba p arba a
+    const text = document.createTextNode(t); // sukuria teksta
+    const root = document.querySelector('#root'); // surandam vieta
+    root.appendChild(div); // pridedam i matoma narsykleje vieta
+    div.appendChild(text); // pridedam teksta i elementa
+    div.classList.add(className);
+}
+
+
+const data = ['Labas, Bebrai', 'Nu sveikutis, Barsukai', 'Kur aini?', 'Namo ainu'];
+
+// const a = salyga ? taip : ne;
+
+setState(true);
 
