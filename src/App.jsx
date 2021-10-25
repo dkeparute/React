99c8d74c496d49d2acd49f29859f505db801537e
Dovilė <dovile.keparute@gmail.com>
2021-10-25 16:51:13 +0300

    Ketvirtadienio paskaita

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,22 @@ import idGenerator from "./Common/idGenerator";
 function App() {
 
     const [field, setField] = useState([]);
-    // kontroliuojamas STATE
+
     const [fieldNumber, setFieldNumber] = useState(1);
 
-    // Add klausia ka prideti (what: avi ar karve)
+
     const add = (what) => {
-        // pasidarom field kopija nes state busenos tiesiogiai negalime keist
+
         const fieldCopy = field.slice();
-        // tada pridedame karve arba avi  i field kopija
+
         fieldCopy.push({
             id: idGenerator(),
             animal: what,
             field: parseInt(fieldNumber)
         });
-        // atiduojam field kopija
+
         setField(fieldCopy);
-        // tuo paciu pasetinam i local storaga
+        // NORINT JOG NEISITRINTU INFORMACIJA
         localStorage.setItem('animals', JSON.stringify(fieldCopy));
         console.log(fieldCopy);
     }
@@ -28,18 +28,15 @@ function App() {
     const selectField = event => {
         setFieldNumber(event.target.value);
     }
-
+// NORINT JOG PASPAUDUS ISSITRINTU GYVULYS
     const goHome = (id) => {
         const fieldCopy = field.slice();
-        fieldCopy.forEach((a, i) => {
-            if (a.id === id) {
-                fieldCopy.splice(i, 1);
-            }
-        });
+        const ind= fieldCopy.findIndex(e => e.id === id);
+        fieldCopy.splice(ind, 1);
         setField(fieldCopy);
         localStorage.setItem('animals', JSON.stringify(fieldCopy));
     }
-
+// NORINT JOG PASPAUDUS ISVALYTU VISA GRUPE GYVULIU
     const groupGoHome = (group) => {
         const fieldCopy = field.slice();
         while (true) {
@@ -55,9 +52,11 @@ function App() {
 
 
     useEffect(() => {
-        // klausiam ar ka nors turi is gyvuliu
+        //  klausiame ar tu turi kokiu nors gyvuliu? Atsakymas bus null - nieko nera
         const animalsfromStorage = localStorage.getItem('animals');
+        // jeigu yra gyvuliu, tada rasome:
         if (null !== animalsfromStorage) {
+            // ir pasetinam i STATE
             setField(JSON.parse(animalsfromStorage));
         }
     }, []);
@@ -67,30 +66,29 @@ function App() {
         <>
             <div className='field'>
                 <div className='field_part'>
-                    {/* sukuriamas naujas komponentas GroupAnimal */}
-                    {field.map((fieldAnimal, index) => <GroupAnimal key={index} field={1} fieldAnimal={fieldAnimal} goHome={goHome}></GroupAnimal>)}
+
+                    {field.map((fieldAnimal, i) => <GroupAnimal key={i} field={1} fieldAnimal={fieldAnimal} goHome={goHome}></GroupAnimal>)}
                 </div>
                 <div className='field_part'>
-                    {/* sukuriamas naujas komponentas GroupAnimal */}
-                    {field.map((fieldAnimal, index) => <GroupAnimal key={index} field={2} fieldAnimal={fieldAnimal} goHome={goHome}></GroupAnimal>)}
+
+                    {field.map((fieldAnimal, i) => <GroupAnimal key={i} field={2} fieldAnimal={fieldAnimal} goHome={goHome}></GroupAnimal>)}
                 </div>
                 <div className='field_part'>
-                    {/* sukuriamas naujas komponentas GroupAnimal */}
-                    {field.map((fieldAnimal, index) => <GroupAnimal key={index} field={3} fieldAnimal={fieldAnimal} goHome={goHome}></GroupAnimal>)}
+
+                    {field.map((fieldAnimal, i) => <GroupAnimal key={i} field={3} fieldAnimal={fieldAnimal} goHome={goHome}></GroupAnimal>)}
                 </div>
             </div>
-
+            <select value={fieldNumber} onChange={selectField}>
+                <option value={1}>Field One</option>
+                <option value={2}>Field Two</option>
+                <option value={3}>Field Three</option>
+            </select>
             <div>
                 <div className='buttons-holder'>
                     <button onClick={() => add('cow')}>Add cow</button>
                     <button onClick={() => add('sheep')}>Add sheep</button>
                     <button onClick={() => add('horse')}>Add horse</button>
                 </div>
-                <select value={fieldNumber} onChange={selectField}>
-                    <option value={1}>Field One</option>
-                    <option value={2}>Field Two</option>
-                    <option value={3}>Field Three</option>
-                </select>
             </div>
             <div className='buttons-holder'>
                 <button onClick={() => groupGoHome('cow')}>Home cow</button>
