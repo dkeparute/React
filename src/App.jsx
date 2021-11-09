6f0c4a2770c7a91e6e24f200df8b930f44dd9020
Dovilė <dovile.keparute@gmail.com>
2021-11-09 11:07:40 +0200

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+// 1. importuojame
+import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
+import RouterBasic from "./Components/RouterBasic";
+
 function App() {
 
-    return(
-        <div>
-            Hello there!
-        </div>
+
+
+    return (
+        //2. parasome router
+        <Router>
+            <h1>Hello there!</h1>
+            <ul className='hello-meniu'>
+                <li>
+                    <Link to='/'>Basic</Link>
+                    <Link to='/africa'>Africa</Link>
+                    <Link to='/australia'>Australia</Link>
+                    <Link to='/antarctica'>Antarctica</Link>
+                </li>
+            </ul>
+            <Routes>
+                <Route path='/' element={<RouterBasic/>}>
+                </Route>
+            </Routes>
+
+        </Router>
     );
-    export default App;
 }
+export default App;
---
+++ src/Components/RouterBasic.jsx
@@ -0,0 +1,13 @@
+
+
+function RouterBasic() {
+
+
+
+    return (
+        <div>
+            <h2>Basic Hello</h2>
+        </div>
+    );
+}
+export default RouterBasic;
