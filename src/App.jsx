1f03a6377693d38e66a52d84a818452ab0a08bfc
Dovilė <dovile.keparute@gmail.com>
2021-11-16 20:51:24 +0200

    reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import Create from "./Components/Domino/Create";
+
 function App() {
 
     return (
         <div className='domino'>
             <h1>Let's play Domino</h1>
+            <Create />
         </div>
     );
 }
--- src/Components/Domino/Create.jsx
+++ src/Components/Domino/Create.jsx
@@ -1,8 +1,24 @@
+import { useReducer } from "react";
+
+
 function Create() {
 
-    return(
-        <div className='domino_create'>
+const [plate, dispatchPlate] = useReducer(dominoPlateReducer, {left: 0, rigt: 0});
+ 
 
+
+    return (
+        <div className='domino_create'>
+            <div className='domino_create_header'>
+                Create new DOMINO plate
+            </div>
+            <div className='domino_create_body'>
+                <div>
+                    <input type="text" onChange={() => handleInput('left')}/>
+                    <input type="text" onChange={() => handleInput('right')} />
+                </div>
+                <button>Create</button>
+            </div>
         </div>
     );
 }
