8acbf97f02a33b14c40aa3ec9cada0fa348ae013
Dovilė <dovile.keparute@gmail.com>
2021-11-16 21:43:10 +0200

    domino reducer

--- src/Reducer/dominoPlateReducer.js
+++ src/Reducer/dominoPlateReducer.js
@@ -1,1 +1,18 @@
-// 5.
+import { ADD_LEFT, ADD_RIGHT } from "../Contstants/dominoTypes";
+
+// 5. jis gauna state ir action objekta ir grazina nauja state
+export function dominoPlateReducer(state, action) {
+    // kadangi bus keiciama dalis steito, tada tikslinga pasidaryti kopija
+    const newState = { ...state };
+    // tada svitchina action tipa
+    switch (action.type) {
+        case ADD_LEFT:
+            newState.left = action.payload;
+            break;
+        case ADD_RIGHT:
+            newState.right = action.payload;
+            break;
+            // kad nepyktu reaktas
+        default:
+    }
+}
